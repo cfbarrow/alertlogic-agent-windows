3e383b0ea221d1f5bc52a575b404b31fe35316c5
Fondop, Clyde <clyde.fondop@ft.com>
2016-10-20 16:31:21 +0100

    Extract dll then execute alagent + refractor on AWS variable

---
+++ main.py
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+
+namespace ftalertlogicagent
+{
+	public class main
+	{
+
+		public static void Main()
+		{
+			List<string> DLL_NAMES = new List<string>() { "Heijden.Dns.dll","Newtonsoft.Json.dll","Newtonsoft.Json.FSharp.dll","FSharp.Core.dll","NodaTime.dll"};
+			load_dll ("ftalertlogicagent.Resources",DLL_NAMES);
+			Deploy.Alagent();
+
+		}
+
+		public static void load_dll(string NameSpace, List<string> dlls){
+			foreach (string dll in dlls) {
+				WriteResourceToFile(NameSpace + "." + dll,dll);	
+			}
+		}
+
+		// Extract file from exe to directory
+		public static void WriteResourceToFile(string resourceName, string fileName)
+		{
+			using(var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+			{
+				using(var file = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+				{
+					resource.CopyTo(file);
+				} 
+			}
+		}
+	}
+}
+
